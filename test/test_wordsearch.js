96505558bb41b7caba359aa6e37035373a500e02
tonyloba <tonyloba@gmail.com>
2021-09-22 23:20:58 +0000

    created 2 files code and test

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,84 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return false for vertical word vertical", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'PIZZA')
+
+    assert.isFalse(result);
+  });
+  
+  it("should return true if the word is present in Vertical", function() {
+    const result = wordSearch([
+      ['F', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['R', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['A', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['N', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['C', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['H', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['I', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['S', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANCHISE')
+     
+    assert.isTrue(result);
+  });
+  it("should return false if the casese are different vertical word vertical", function() {
+      const result = wordSearch([
+      ['f', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'fRANK')
+
+  assert.isFalse(result);
+});
+
+  it("should return true if the cases are same and word is present in Vertical", function() {
+    const result = wordSearch([
+      ['f', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['R', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['A', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['N', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['C', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['H', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['I', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['S', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'fRANCHISE')
+     
+    assert.isTrue(result);
+  });
+
+  it("should return true if the cases are differrent and word is present in Vertical", function() {
+    const result = wordSearch([
+      ['F', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['R', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['A', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['N', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['C', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['H', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['I', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['S', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'fRANCHISE')
+     
+    assert.isTrue(result);
+  });
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,64 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+// helper functions
+// horizontalCheck
+// verticalCheck
+const horizontalCheck = function (letters, word) {
+
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (let l of horizontalJoin) {
+        // console.log('each line:', l);
+        if (l.toLowerCase().includes(word.toLowerCase())) {   
+            return true;
+        }
     }
+    // return false if horizontal and vertical are not true;
+    return false;
+}
+
+const transpose = function (matrix) {
+    let newArray = [];
+    //number of arrays in the matrix    
+    for (let i = 0; i < matrix[0].length; i++) {
+        newArray.push([])
+    }
+
+    for (let j = 0; j < matrix.length; j++) {
+        for (let k = 0; k < matrix[j].length; k++) {
+            console.log(matrix[k]);
+            newArray[k].push(matrix[j][k]);
+
+        }
+    }
+
+    return newArray;
+}
+
+
+
+
+const verticalCheck = function (letters, word) {
+    //transpose the letters matrix:
+    return horizontalCheck(transpose(letters), word);
+    //then do the samething as horizontalJoin
+
+}
+
+
+
+const wordSearch = (letters, word) => {
+// if (horizontalCheck(letters, word) === true || verticalCheck(letters, word) === true) {
+//     return true;
+// } else {
+//     return false;
+// }
+
+
+  if (horizontalCheck(letters, word)) {
+      return true;
+  }
+  if (verticalCheck(letters, word)) {
+      return true;
+  }
+  return false;
 }
 
 module.exports = wordSearch
